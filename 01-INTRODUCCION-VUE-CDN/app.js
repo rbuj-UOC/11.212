95dd199514b28f3d4bc20c5052bba6b6a36014cc
Robert Buj <robert.buj@gmail.com>
2026-04-16 19:18:59 +0200

    3rd attempt to change the name timeout -> button

--- 01-INTRODUCCION-VUE-CDN/app.js
+++ 01-INTRODUCCION-VUE-CDN/app.js
@@ -15,20 +15,35 @@ const app = createApp({
     template: `
         <h1>Hello world from Vue App!</h1>
         <h2>{{name}}</h2>
+        <button @click="updateName">Change name</button>
     `,
     // Paso 3.1
     // Añadimos la función setup, definido la variable name y la retornamos
     // para que sea accesible desde el template
     setup() {
         let name = ref("Robert");
+
+        /*
         // Paso 3.2
         // Modificamos el valor de name después de 2 segundos
         setTimeout(() => {
             name.value = "Antoni";
             console.log("Change value Robert to Antoni");
         }, 2000);
+        */
+
+        // Paso 4
+        // creamos la función updateName para cambiar el valor de name
+        const updateName = () => {
+            name.value = "Antoni";
+            console.log("Change value Robert to Antoni");
+        }
+
         return {
-            name
+            name,
+            // Paso 4
+            // Retornamos la función updateName para que sea accesible desde el template
+            updateName
         }
     }
 });
