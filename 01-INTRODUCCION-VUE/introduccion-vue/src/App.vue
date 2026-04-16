549750bc6debb9856dea7b9585cbb73369c871dd
Robert Buj <robert.buj@gmail.com>
2026-04-16 21:04:34 +0200

    T4. U1 - Paso 3: Hello world en un SFC

--- 01-INTRODUCCION-VUE/introduccion-vue/src/App.vue
+++ 01-INTRODUCCION-VUE/introduccion-vue/src/App.vue
@@ -1,11 +1,17 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+console.log('Hello world from script (Vite + TS)');
+</script>
 
 <template>
-  <h1>You did it!</h1>
-  <p>
-    Visit <a href="https://vuejs.org/" target="_blank" rel="noopener">vuejs.org</a> to read the
-    documentation
-  </p>
+  <main style="padding: 1rem">
+    <h1>Hello world from Vue App (Vite + TS)</h1>
+    <p>Hello world from template</p>
+  </main>
 </template>
 
-<style scoped></style>
+<style scoped>
+/* Estilo global aplicado a toda la app */
+body {
+  font-family: Arial, sans-serif;
+}
+</style>
