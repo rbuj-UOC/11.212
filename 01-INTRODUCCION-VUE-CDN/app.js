7b1b0e8597a2a2bf2801675b31226de70e333744
Robert Buj <robert.buj@gmail.com>
2026-04-16 19:02:26 +0200

    Display name in template

--- 01-INTRODUCCION-VUE-CDN/app.js
+++ 01-INTRODUCCION-VUE-CDN/app.js
@@ -10,6 +10,18 @@ console.log("Vue object --> ", Vue);
 // Creamos la aplicación Vue
 const { createApp } = Vue;
 const app = createApp({
-    template: `<h1>Hello world from Vue App!</h1>`
+    template: `
+        <h1>Hello world from Vue App!</h1>
+        <h2>{{name}}</h2>
+    `,
+    // Paso 3.1
+    // Añadimos la función setup, definido la variable name y la retornamos
+    // para que sea accesible desde el template
+    setup() {
+        const name = "Robert";
+        return {
+            name
+        }
+    }
 });
 app.mount('#vueApp');
