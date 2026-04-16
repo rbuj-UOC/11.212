563ebd46a71edb0b9bb21717a7e1573b4e2d12d5
Robert Buj <robert.buj@gmail.com>
2026-04-16 18:53:53 +0200

    create Vue app

--- 01-INTRODUCCION-VUE-CDN/app.js
+++ 01-INTRODUCCION-VUE-CDN/app.js
@@ -1,2 +1,15 @@
 console.log('Hello world!');
 console.log("Hello world from script");
+
+// Paso 2
+// console.log del objeto Vue
+console.log("Vue object --> ", Vue);
+
+
+// Paso 2.3
+// Creamos la aplicación Vue
+const { createApp } = Vue;
+const app = createApp({
+    template: `<h1>Hello world from Vue App!</h1>`
+});
+app.mount('#vueApp');
