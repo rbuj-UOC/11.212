7065f149991ad19ff53c875be02fd4901cfdfbed
Robert Buj <robert.buj@gmail.com>
2026-04-18 09:50:43 +0200

    T4. U3 - Paso 4: Implementación del formulario con botón de envío (submit)

--- 04-FORMULARIOS-WEB/formularios-web/src/components/PrimerFormulario.vue
+++ 04-FORMULARIOS-WEB/formularios-web/src/components/PrimerFormulario.vue
@@ -1,9 +1,11 @@
 <template>
-  <section class="card">
+  <form class="card" @submit.prevent="onSubmit">
     <h2>Primer formulario</h2>
+    <label for="email">Email: </label>
     <input type="text" v-model="email" v-bind="emailAttrs" placeholder="tu@email.com" />
+    <button class="btn" type="submit">Enviar</button>
     <pre>{{ values }}</pre>
-  </section>
+  </form>
 </template>
 
 <script setup lang="ts">
@@ -11,6 +13,10 @@ import { useForm } from 'vee-validate'
 
 const { values, defineField } = useForm()
 const [email, emailAttrs] = defineField('email')
+
+function onSubmit() {
+  console.log('Submit:', JSON.stringify(values))
+}
 </script>
 
 <style scoped>
