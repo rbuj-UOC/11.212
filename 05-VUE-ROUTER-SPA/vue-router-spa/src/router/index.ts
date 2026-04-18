8a53eb1dabd96317a3ea9b2a50cbdec49a183e59
Robert Buj <robert.buj@gmail.com>
2026-04-18 17:25:20 +0200

    T4. U4 - Paso 3: Definición de las primeras rutas

--- 05-VUE-ROUTER-SPA/vue-router-spa/src/router/index.ts
+++ 05-VUE-ROUTER-SPA/vue-router-spa/src/router/index.ts
@@ -1,8 +1,24 @@
+import HomePage from "@/modules/landing/pages/HomePage.vue";
 import { createRouter, createWebHistory } from "vue-router";
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
-  routes: [],
+  routes: [
+    {
+      path: "/",
+      component: HomePage,
+    },
+    {
+      path: "/about",
+      name: "about",
+      component: () => import("@/modules/landing/pages/AboutPage.vue"),
+    },
+    {
+      path: "/contact",
+      name: "contact",
+      component: () => import("@/modules/landing/pages/ContactPage.vue"),
+    },
+  ],
 });
 
 export default router;
